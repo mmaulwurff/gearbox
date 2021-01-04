6b8351edd33e3632a49a8122923c6b89a973a146
Alexander Kromm <mmaulwurff@gmail.com>
2021-01-04 18:01:23 +0700

    add changelog script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,15 @@ mkdir -p build
 
 filename=$(./scripts/make_name.sh)
 
+git log --date=short --pretty=format:"-%d %ad %s%n" | \
+    grep -v "^$" | \
+    sed "s/HEAD -> master, //" | \
+    sed "s/, origin\/master//" | \
+    sed "s/ (HEAD -> master)//" | \
+    sed "s/ (origin\/master)//"  |\
+    sed "s/- (tag: \(v\?[0-9.]*\))/\n\1\n-/" \
+    > changelog.txt
+
 rm -f "$filename"
 zip -R0 "$filename" "*.md" "*.txt" "*.zs" "*.png"
 
